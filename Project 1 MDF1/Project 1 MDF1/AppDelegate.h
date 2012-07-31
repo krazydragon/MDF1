720ac3e0746aedd6af971720b6a602b9594ab2ef
Ronaldo Barnes <krazydragon@fullsail.edu>
2012-07-31 16:25:39 -0700

    Started Project 1

---
+++ Project 1 MDF1/Project 1 MDF1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Project 1 MDF1
+//
+//  Created by Ronaldo Barnes on 7/31/12.
+//  Copyright (c) 2012 Ronaldo Barnes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Project 1 MDF1/Project 1 MDF1/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  Project 1 MDF1
+//
+//  Created by Ronaldo Barnes on 7/31/12.
+//  Copyright (c) 2012 Ronaldo Barnes. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+
+
+@end
